e221f399b8ffbe1cf72c449f5e954cc57e0afcc2
Shyam <shyam.b@simformsolutions.com>
2023-06-01 10:01:49 +0530

    Auto layout initial commit..

---
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by Shyam Buhecha on 01/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
