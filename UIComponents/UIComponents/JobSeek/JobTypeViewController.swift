5d3385900d0e4419f51c59113a61bc9a3065d108
Shyam <shyam.b@simformsolutions.com>
2023-06-15 21:03:38 +0530

    phpicker and scrollview

---
+++ UIComponents/UIComponents/JobSeek/JobTypeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  JobTypeViewController.swift
+//  UIComponents
+//
+//  Created by Shyam Buhecha on 15/06/23.
+//
+
+import UIKit
+
+class JobTypeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ UIComponents/UIComponents/controllers/ScrollViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ScrollViewController.swift
+//  UIComponents
+//
+//  Created by Shyam Buhecha on 15/06/23.
+//
+
+import UIKit
+
+class ScrollViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
