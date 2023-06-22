456b937cbdb8f0f384d5e6b9f842b2a4ef99f38e
Shyam <shyam.b@simformsolutions.com>
2023-06-22 15:55:15 +0530

    UIComponents KT Screen

--- ktproject/ktproject/ViewController.swift
+++ ktproject/ktproject/ViewController.swift
@@ -1,102 +1,125 @@
-//
-//  ViewController.swift
-//  ktproject
-//
-//  Created by Shyam Buhecha on 21/06/23.
-//
-
 import UIKit
 
 struct data {
     let settingImg: String
     let settingName: String
-    let btnStatus: Bool
+    let btnStatusImg: String
 }
-class ViewController: UIViewController, UITableViewDelegate , UITableViewDataSource {
+class ViewController: UIViewController {
 
-    var firstSectionList = [data(settingImg: "img", settingName: "User Face Id for Login",btnStatus: true),data(settingImg: "secImg", settingName: "User Face Id for Login",btnStatus: false)]
-    var secondSectionList = [data(settingImg: "menu", settingName: "User Face Id for Login",btnStatus: true),data(settingImg: "img4", settingName: "User Face Id for Login",btnStatus: false)]
-
-    let sectionData = [[data(settingImg: "img", settingName: "User Face Id for Login",btnStatus: true),data(settingImg: "secImg", settingName: "User Face Id for Login",btnStatus: false)],[data(settingImg: "menu", settingName: "User Face Id for Login",btnStatus: true),data(settingImg: "ring", settingName: "User Face Id for Login",btnStatus: false)]]
-
+    let sectionData = [[data(settingImg: Images.img, settingName: SettingsTitles.title1,btnStatusImg: Images.on),data(settingImg: Images.secImg, settingName: SettingsTitles.title2,btnStatusImg: Images.on)],[data(settingImg: Images.menu, settingName:SettingsTitles.title3,btnStatusImg: Images.off),data(settingImg: Images.ring, settingName: SettingsTitles.title4,btnStatusImg: Images.on)]]
 
     @IBOutlet weak var tblSetting: UITableView!
-   
     @IBOutlet weak var btnCancle: UIView!
-    @IBOutlet weak var bottumView: UIView!
+    @IBOutlet weak var bottomView: UIView!
+    @IBOutlet weak var btnHelp: UIButton!
+    
+    @IBOutlet var vc: UIView!
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
    
         tblSetting.delegate = self
         tblSetting.dataSource = self
-        tblSetting.register(UINib(nibName: "TableViewCell", bundle: nil), forCellReuseIdentifier: "TableViewCell")
-        
-        bottumView.layer.cornerRadius = 24
+        tblSetting.register(UINib(nibName: NibName.tableViewCell, bundle: nil), forCellReuseIdentifier: NibName.tableViewCell)
         
         btnCancle.layer.borderWidth = 1
         btnCancle.layer.borderColor = CGColor.init(red: CGFloat(0), green:  CGFloat(0), blue:  CGFloat(0), alpha:  1)
         btnCancle.layer.cornerRadius = 8
         
-    }
-    
-    //Delegate
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
-    }
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        self.navigationItem.title = Constant.setting
         
-        return sectionData[section].count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
+        bottomView.layer.cornerRadius = 24
+        bottomView.isHidden = true
+        
+        let backButton = UIBarButtonItem(title: "", style: .plain, target: self, action: nil    )
+       
+        backButton.setBackgroundImage(UIImage(named: Images.icon), for: .normal, barMetrics: .default) 
+        self.navigationItem.setLeftBarButton(backButton, animated: false)
+   
+    }
 
-         let cell =  tblSetting.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath) as! TableViewCell
-        
-        let oneCell = sectionData[indexPath.section][indexPath.row]
-        cell.settingImg.image = UIImage(named: oneCell.settingImg)
-        cell.lblSettingTitle.text = oneCell.settingName
-        cell.btn.setBackgroundImage(UIImage(named: "on"), for: .normal)
-        cell.btn.tintColor = UIColor.white
-        cell.btn.setBackgroundImage(UIImage(named: "off"), for: .highlighted)
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-            let headerView = UIView.init(frame: CGRect.init(x: 0, y: 0, width: tableView.frame.width, height: 50))
-            
-            let label = UILabel()
-            label.frame = CGRect.init(x: 5, y: 5, width: headerView.frame.width-10, height: headerView.frame.height-10)
-        if section == 0 {
-            label.text = "Privacy"
-        }else {
-            label.text = "Other"
-        }
-           
-            label.font = .systemFont(ofSize: 16)
-            label.textColor = .black
-        headerView.backgroundColor = UIColor(named: "lightblack")
-            headerView.addSubview(label)
-        
-            return headerView
-        }
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-            return 50
-        }
     var num = true
     @IBAction func clickOnButton(_ sender: UIButton) {
        
         if num {
-            sender.setImage(UIImage(named: "on"), for: .normal)
-            bottumView.isHidden = false
+            sender.setImage(UIImage(named: Images.on), for: .normal)
+            bottomView.isHidden = false
+            
             num.toggle()
         }else {
-            sender.setImage(UIImage(named: "off"), for: .normal)
-            bottumView.isHidden = true
+            sender.setImage(UIImage(named: Images.off), for: .normal)
+            bottomView.isHidden = true
             num.toggle()
         }
+    }
+    
+}
+
+extension ViewController: UITableViewDataSource{
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sectionData[section].count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell =  tblSetting.dequeueReusableCell(withIdentifier: NibName.tableViewCell, for: indexPath) as! TableViewCell
+        let oneCell = sectionData[indexPath.section][indexPath.row]
+        cell.settingImg.image = UIImage(named: oneCell.settingImg)
+        cell.lblSettingTitle.text = oneCell.settingName
+        cell.btn.tintColor = UIColor.white
+        cell.btn.setImage(UIImage(named: oneCell.btnStatusImg), for: .normal)
+      
        
-       
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+            let headerView = UIView.init(frame: CGRect.init(x: 0, y: 0, width: tableView.frame.width, height: 40))
+            headerView.layer.cornerRadius = 8
+            
+            let label = UILabel()
+            label.frame = CGRect.init(x: 5, y: 5, width: headerView.frame.width-10, height: headerView.frame.height-10)
+        if section == 0 {
+            label.text = Text.text_privacy
+        }else {
+            label.text = Text.text_other
+        }
+           
+            label.font = .systemFont(ofSize: 16)
+            label.textColor = .black
+        headerView.backgroundColor = UIColor(named: Images.lightblack)
+            headerView.addSubview(label)
+        
+            return headerView
+        }
+ 
+}
+
+extension ViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+            return 40
+        }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You selected cell #\(indexPath.row)!")
+    }
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        print("You deselected cell #\(indexPath.row)!")
     }
 }
+
+
+
+
+
 
