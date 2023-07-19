ace5ffb0edf9da6947ab06fb0a7ba55d98e18dd6
Shyam <shyam.b@simformsolutions.com>
2023-07-19 16:11:35 +0530

    feature ui componentskt

---
+++ ktproject/ktproject/Constants/Constant.swift
@@ -0,0 +1,41 @@
+import Foundation
+
+struct Constant {
+    static let setting = "Settings"
+}
+
+struct Images {
+    static let on = "on"
+    static let off = "off"
+    static let icon = "icon"
+    static let img = "img"
+    static let lightblack = "lightblack"
+    static let menu1 = "Menu 1"
+    static let menu = "Menu"
+    static let ring = "ring"
+    static let secImg = "secImg"
+    static let thumpimage = "thumpimage"
+}
+
+struct SettingsTitles {
+    static let title1 = "Use Face ID for Login"
+    static let title2 = "Use Touch ID for Login"
+    static let title3 = "Dark Theme"
+    static let title4 = "Notification"
+}
+
+struct Text {
+    static let text_privacy = "Privacy"
+    static let text_other = "Other"
+}
+
+struct NibName {
+    static let tableViewCell = "TableViewCell"
+}
+
+struct BottomSheetConstants {
+    static let cancel = "Cancel"
+    static let title = "Switch Account"
+    static let switchNow = "Switch Now"
+    static let message = "You are about to switch to valet account. Are you sure you want switch?"
+}
